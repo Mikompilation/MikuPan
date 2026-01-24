49b21af37808f822f200022553af121d9c5c998c
weirdbeardgame <obarrtimothy@gmail.com>
2026-01-23 17:39:19 -0700

    Restored old ReadFileInArchive

--- src/mikupan/mikupan_file_c.h
+++ src/mikupan/mikupan_file_c.h
@@ -5,6 +5,7 @@
 void MikuPan_LoadImgHdFile();
 void MikuPan_ReadFullFile(const char *filename, char* buffer);
 void MikuPan_ReadFileInArchive(int sector, int size, u_int *address);
+void MikuPan_ReadFileInArchive64(int sector, int size, int64_t address);
 u_int MikuPan_GetFileSize(const char *filename);
 
 #endif//MIKUPAN_MIKUPAN_FILE_C_H
