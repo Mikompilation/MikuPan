8b4a61e57fcee5a762b3a98b330991f0390957ba
William Adam-Grenier <william.a-g@hotmail.com>
2026-05-28 22:24:55 -0400

    Restore IOP Thread Wait Value

--- src/iop/iopmain.c
+++ src/iop/iopmain.c
@@ -220,7 +220,7 @@ static int IopInitMain()
 static SDLCALL int IopMain(void *data)
 {
     Uint64 nextTick = SDL_GetTicksNS();
-    const Uint64 interval = 1;
+    const Uint64 interval = 4167000;
 
     while (!request_shutdown)
     {
